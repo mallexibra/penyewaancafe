3b81378068789adc3e046f2e74bf2b8be7c4d749
mallexibra <mallexibra@gmail.com>
2023-06-10 19:24:32 +0700

    fix: Memperbaiki toggle navbar pada halaman admin

--- src/style/script.js
+++ src/style/script.js
@@ -5,9 +5,3 @@ burger.addEventListener("click", () => {
   sidebar.classList.toggle("hidden");
   sidebar.classList.toggle("flex");
 });
-
-const arrowNavAdmin = document.getElementById("arrow_nav_admin");
-const navAdmin = document.getElementById("nav_admin");
-arrowNavAdmin.addEventListener("click", () => {
-  navAdmin.classList.toggle("hidden");
-});
