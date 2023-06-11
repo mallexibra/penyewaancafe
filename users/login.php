d9821f63bd0030e873a82d5a9164aee98f719744
mallexibra <mallexibra@gmail.com>
2023-06-11 11:19:51 +0700

    add: Menambahkan script PHP pada halaman produk login dan signup

---
+++ users/login.php
@@ -0,0 +1,93 @@
+<?php
+
+session_start();
+include '../src/model/conDB.php';
+$query = mysqli_query($mysqli, "SELECT * FROM user");
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Login Users</title>
+  <link rel="stylesheet" href="../src/style/style.css" />
+</head>
+
+<body class="font-poppins">
+  <nav class="bg-stone-800/90 backdrop-blur-sm fixed top-0 z-10 w-full p-4 text-white">
+    <div class="relative flex justify-center md:justify-between items-center mx-auto max-w-4xl">
+      <div class="nav_brand flex justify-center items-center gap-3">
+        <img src="../src/assets/image/cafestuff.png" alt="logo" class="w-6" />
+        <h1 class="sm:text-2xl text-xl font-extrabold">CAFESTUFF</h1>
+      </div>
+      <img class="absolute right-0 w-9 md:hidden cursor-pointer" src="../src/assets/icons/burger_menu.png" alt="burger_menu" id="burger_menu" />
+      <div id="sidebar" class="list_item text-sm font-medium fixed md:static md:bg-transparent bg-stone-800/90 w-[70%] h-screen md:h-auto top-[60px] right-0 hidden md:flex md:flex-row flex-col md:justify-end justify-center items-center gap-7">
+        <ul class="flex flex-col md:flex-row text-center gap-7">
+          <li>
+            <a class="hover:scale-110 text-sm inline-block" href="../index.html">Dashboard</a>
+          </li>
+          <li>
+            <a class="hover:scale-110 text-sm inline-block" href="produk.html">Sewa Barang</a>
+          </li>
+          <li>
+            <a class="hover:scale-110 text-sm inline-block" href="keranjang.html">Keranjang</a>
+          </li>
+        </ul>
+        <div class="btn flex md:flex-row flex-col md:flex-wrap text-center gap-7">
+          <a class="bg-yellow-400 hover:bg-transparent hover:text-yellow-400 transition-all ease-in-out duration-150 border-2 border-yellow-400 px-3 py-1 rounded-sm text-stone-800 font-semibold" href="signup.html">Sign Up</a>
+          <a class="px-3 py-1 text-yellow-400 hover:bg-yellow-400 hover:text-stone-800 font-semibold transition-all ease-in-out duration-150 border-2 border-yellow-400 rounded-sm" href="login.html">Login</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <main class="w-full min-h-screen bg-yellow-100 grid place-items-center">
+    <form action="#" method="post" class="flex flex-col mt-20 w-4/5 md:w-5/12 bg-stone-700 text-white p-6 rounded-md shadow-lg">
+      <div class="flex w-full mb-3 text-center font-semibold justify-between">
+        <a href="login.html" class="w-full block bg-yellow-500 text-stone-700 rounded-sm p-2">LOGIN</a>
+        <a href="signup.html" class="w-full block rounded-sm p-2">SIGN UP</a>
+      </div>
+      <h1 class="text-center font-extrabold text-2xl">LOGIN USERS</h1>
+      <p class="text-center text-xs my-3">Silahkan login dengan akun anda!</p>
+      <label for="username" class="flex flex-col mt-5 mb-3">
+        <span class="font-semibold">Username</span>
+        <input type="text" class="p-2 text-white text-xs bg-white/30 rounded-sm outline-none ring-2 ring-stone-400" name="username" id="username" placeholder="Username anda ..." />
+      </label>
+      <label for="password" class="flex flex-col mb-3">
+        <span class="font-semibold">Password</span>
+        <input type="password" class="p-2 text-white text-xs bg-white/30 rounded-sm outline-none ring-2 ring-stone-400" name="password" id="password" placeholder="Password anda ..." />
+      </label>
+      <button type="submit" name="login" class="w-full p-3 bg-stone-900 rounded font-bold">
+        Login
+      </button>
+      <a href="../admin/login.php" class="text-center text-xs mt-2 text-yellow-500 italic underline">Login Sebagai Admin</a>
+    </form>
+  </main>
+  <footer class="bg-stone-700 p-3 text-sm text-center text-white">
+    <p class="font-semibold">Copyright &copy; 2023 by Mallexibra</p>
+  </footer>
+
+  <?php
+
+  if (isset($_POST['login'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    while ($row = mysqli_fetch_assoc($query)) {
+      if ($username == $row['username'] && $password == $row['password']) {
+        $_SESSION['user'] = $row['nama'];
+        header("Location: produk.php");
+        exit();
+      }
+    }
+  }
+
+  ?>
+
+  <script src="../src/style/script.js"></script>
+</body>
+
+</html>
---
+++ users/produk.php
@@ -0,0 +1,153 @@
+<?php
+
+session_start();
+include '../src/model/conDB.php';
+
+if (!isset($_SESSION['user'])) {
+  header("Location: login.php");
+  exit();
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Produk</title>
+  <link rel="stylesheet" href="../src/style/style.css" />
+</head>
+
+<body class="font-poppins">
+  <nav class="bg-stone-800/90 backdrop-blur-sm fixed top-0 z-10 w-full p-4 text-white">
+    <div class="relative flex justify-center md:justify-between items-center mx-auto max-w-4xl">
+      <div class="nav_brand flex justify-center items-center gap-3">
+        <img src="../src/assets/image/cafestuff.png" alt="logo" class="w-6" />
+        <h1 class="sm:text-2xl text-xl font-extrabold">CAFESTUFF</h1>
+      </div>
+      <img class="absolute right-0 w-9 md:hidden cursor-pointer" src="../src/assets/icons/burger_menu.png" alt="burger_menu" id="burger_menu" />
+      <div id="sidebar" class="list_item text-sm font-medium fixed md:static md:bg-transparent bg-stone-800/90 w-[70%] h-screen md:h-auto top-[60px] right-0 hidden md:flex md:flex-row flex-col md:justify-end justify-center items-center gap-7">
+        <ul class="flex flex-col md:flex-row text-center gap-7">
+          <li>
+            <a class="hover:scale-110 text-sm inline-block" href="../index.html">Dashboard</a>
+          </li>
+          <li>
+            <a class="hover:scale-110 text-sm inline-block" href="produk.html">Sewa Barang</a>
+          </li>
+          <li>
+            <a class="hover:scale-110 text-sm inline-block" href="keranjang.html">Keranjang</a>
+          </li>
+        </ul>
+        <div class="btn flex md:flex-row flex-col md:flex-wrap text-center gap-7">
+          <a class="bg-yellow-400 hover:bg-transparent hover:text-yellow-400 transition-all ease-in-out duration-150 border-2 border-yellow-400 px-3 py-1 rounded-sm text-stone-800 font-semibold" href="signup.html">Sign Up</a>
+          <a class="px-3 py-1 text-yellow-400 hover:bg-yellow-400 hover:text-stone-800 font-semibold transition-all ease-in-out duration-150 border-2 border-yellow-400 rounded-sm" href="login.html">Login</a>
+        </div>
+      </div>
+    </div>
+  </nav>
+  <main class="mt-16 w-full min-h-screen">
+    <div class="mx-auto max-w-4xl px-3 sm:p-0">
+      <div class="search p-3 flex justify-center items-center gap-3">
+        <input class="bg-stone-700/50 ring-2 ring-stone-700 outline-none placeholder:italic placeholder:text-stone-300 p-2 rounded-md w-3/4" type="text" placeholder="Cari barangmu disini" name="search" id="search" />
+        <button class="bg-stone-700 p-3 text-white rounded-md text-sm">
+          Search
+        </button>
+      </div>
+      <div class="category flex gap-x-3 overflow-x-scroll p-3">
+        <span class="active_category px-3 py-2 ring-2 ring-stone-700 rounded-md">Semua</span>
+        <span class="ring-2 ring-stone-700 rounded-md px-3 py-2">Oven</span>
+        <span class="ring-2 ring-stone-700 rounded-md px-3 py-2">Oven</span>
+        <span class="ring-2 ring-stone-700 rounded-md px-3 py-2">Oven</span>
+        <span class="ring-2 ring-stone-700 rounded-md px-3 py-2">Oven</span>
+        <span class="ring-2 ring-stone-700 rounded-md px-3 py-2">Oven</span>
+        <span class="ring-2 ring-stone-700 rounded-md px-3 py-2">Oven</span>
+        <span class="ring-2 ring-stone-700 rounded-md px-3 py-2">Oven</span>
+        <span class="ring-2 ring-stone-700 rounded-md px-3 py-2">Oven</span>
+        <span class="ring-2 ring-stone-700 rounded-md px-3 py-2">Oven</span>
+        <span class="ring-2 ring-stone-700 rounded-md px-3 py-2">Oven</span>
+        <span class="ring-2 ring-stone-700 rounded-md px-3 py-2">Oven</span>
+        <span class="ring-2 ring-stone-700 rounded-md px-3 py-2">Oven</span>
+        <span class="ring-2 ring-stone-700 rounded-md px-3 py-2">Oven</span>
+        <span class="ring-2 ring-stone-700 rounded-md px-3 py-2">Oven</span>
+        <span class="ring-2 ring-stone-700 rounded-md px-3 py-2">Oven</span>
+        <span class="ring-2 ring-stone-700 rounded-md px-3 py-2">Oven</span>
+      </div>
+      <section class="mt-3 flex gap-4 justify-center flex-wrap">
+        <span class="card inline-flex flex-col bg-stone-500 text-white rounded-md overflow-hidden ring-2 ring-stone-700">
+          <img src="../src/assets/image/card.jpg" alt="" class="w-52 rounded-sm inline-block" />
+          <div class="p-2">
+            <h2 class="font-bold">Hello World!</h2>
+            <p class="text-xs pb-2">Rp. 17.000 / day</p>
+            <a class="bg-stone-800 hover:bg-stone-700 block text-center rounded-md text-xs font-bold p-2" href="#">Lihat Detail</a>
+          </div>
+        </span>
+        <span class="card inline-flex flex-col bg-stone-500 text-white rounded-md overflow-hidden ring-2 ring-stone-700">
+          <img src="../src/assets/image/card.jpg" alt="" class="w-52 rounded-sm inline-block" />
+          <div class="p-2">
+            <h2 class="font-bold">Hello World!</h2>
+            <p class="text-xs pb-2">Rp. 17.000 / day</p>
+            <a class="bg-stone-800 hover:bg-stone-700 block text-center rounded-md text-xs font-bold p-2" href="detailproduk.html">Lihat Detail</a>
+          </div>
+        </span>
+        <span class="card inline-flex flex-col bg-stone-500 text-white rounded-md overflow-hidden ring-2 ring-stone-700">
+          <img src="../src/assets/image/card.jpg" alt="" class="w-52 rounded-sm inline-block" />
+          <div class="p-2">
+            <h2 class="font-bold">Hello World!</h2>
+            <p class="text-xs pb-2">Rp. 17.000 / day</p>
+            <a class="bg-stone-800 hover:bg-stone-700 block text-center rounded-md text-xs font-bold p-2" href="#">Lihat Detail</a>
+          </div>
+        </span>
+        <span class="card inline-flex flex-col bg-stone-500 text-white rounded-md overflow-hidden ring-2 ring-stone-700">
+          <img src="../src/assets/image/card.jpg" alt="" class="w-52 rounded-sm inline-block" />
+          <div class="p-2">
+            <h2 class="font-bold">Hello World!</h2>
+            <p class="text-xs pb-2">Rp. 17.000 / day</p>
+            <a class="bg-stone-800 hover:bg-stone-700 block text-center rounded-md text-xs font-bold p-2" href="#">Lihat Detail</a>
+          </div>
+        </span>
+        <span class="card inline-flex flex-col bg-stone-500 text-white rounded-md overflow-hidden ring-2 ring-stone-700">
+          <img src="../src/assets/image/card.jpg" alt="" class="w-52 rounded-sm inline-block" />
+          <div class="p-2">
+            <h2 class="font-bold">Hello World!</h2>
+            <p class="text-xs pb-2">Rp. 17.000 / day</p>
+            <a class="bg-stone-800 hover:bg-stone-700 block text-center rounded-md text-xs font-bold p-2" href="#">Lihat Detail</a>
+          </div>
+        </span>
+        <span class="card inline-flex flex-col bg-stone-500 text-white rounded-md overflow-hidden ring-2 ring-stone-700">
+          <img src="../src/assets/image/card.jpg" alt="" class="w-52 rounded-sm inline-block" />
+          <div class="p-2">
+            <h2 class="font-bold">Hello World!</h2>
+            <p class="text-xs pb-2">Rp. 17.000 / day</p>
+            <a class="bg-stone-800 hover:bg-stone-700 block text-center rounded-md text-xs font-bold p-2" href="#">Lihat Detail</a>
+          </div>
+        </span>
+        <span class="card inline-flex flex-col bg-stone-500 text-white rounded-md overflow-hidden ring-2 ring-stone-700">
+          <img src="../src/assets/image/card.jpg" alt="" class="w-52 rounded-sm inline-block" />
+          <div class="p-2">
+            <h2 class="font-bold">Hello World!</h2>
+            <p class="text-xs pb-2">Rp. 17.000 / day</p>
+            <a class="bg-stone-800 hover:bg-stone-700 block text-center rounded-md text-xs font-bold p-2" href="#">Lihat Detail</a>
+          </div>
+        </span>
+        <span class="card inline-flex flex-col bg-stone-500 text-white rounded-md overflow-hidden ring-2 ring-stone-700">
+          <img src="../src/assets/image/card.jpg" alt="" class="w-52 rounded-sm inline-block" />
+          <div class="p-2">
+            <h2 class="font-bold">Hello World!</h2>
+            <p class="text-xs pb-2">Rp. 17.000 / day</p>
+            <a class="bg-stone-800 hover:bg-stone-700 block text-center rounded-md text-xs font-bold p-2" href="#">Lihat Detail</a>
+          </div>
+        </span>
+      </section>
+    </div>
+  </main>
+  <footer class="bg-stone-700 p-3 text-sm text-center text-white mt-6">
+    <p class="font-semibold">Copyright &copy; 2023 by Mallexibra</p>
+  </footer>
+
+  <script src="../src/style/script.js"></script>
+</body>
+
+</html>
